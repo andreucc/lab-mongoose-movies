a33ff6d62947de4325d72119cdf4f804ac718e6e
andreucc <andreucc@protonmail.com>
2019-03-02 19:40:01 +0100

    Iteration 3 finish - existential doubt in celebrities.js:18

--- routes/celebrities.js
+++ routes/celebrities.js
@@ -5,10 +5,22 @@ const router = express.Router();
 
 const Celebrity = require('../models/Celebrity');
 
+// GET CELEBRITIES LIST
 router.get('/', async (req, res, next) => {
   try {
     const celebrities = await Celebrity.find();
-    res.render('../views/celebrities/index.hbs', { celebrities });
+    res.render('../views/celebrities/index', { celebrities });
+  } catch (error) {
+    next(error);
+  }
+});
+
+// GET CELEBRITY DETAIL **He tenido que añadir details a la ruta ya que con "/:id" y otras pruebas que he echo no conseguia enrutar bien**
+router.get('/details/:id', async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const celebrity = await Celebrity.findById(id);
+    res.render('../views/celebrities/show', celebrity);
   } catch (error) {
     next(error);
   }
